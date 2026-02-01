67cd3a5fbdbc3ce20d0a9e86a0938eb69228269f
alexisrperry <pinkninja1109@gmail.com>
2026-02-01 00:10:09 -0500

    Moved pages around, deleted github repo page

--- 1_Home.py
+++ 1_Home.py
@@ -17,7 +17,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown('<div class="nav-wrap">', unsafe_allow_html=True)
-c1, c2, c3, c4, c5 = st.columns([1,1,1,1,1], gap="small")
+c1, c2, c3, c4 = st.columns([1,1,1,1], gap="small")
 with c1:
     if st.button("🏠 Home", use_container_width=True):
         st.switch_page("1_Home.py")
@@ -25,14 +25,11 @@ with c2:
     if st.button("🎥 Demo", use_container_width=True):
         st.switch_page("pages/2_DemoVideo.py")
 with c3:
-    if st.button("👥 About Us", use_container_width=True):
-        st.switch_page("pages/3_Info.py")
-with c4:
     if st.button("🔍 CodeOrigin", use_container_width=True):
         st.switch_page("pages/4_CommitAnalysis.py")
-with c5:
-    if st.button("⚙ Repository Link", use_container_width=True):
-        st.switch_page("pages/5_RepoLink.py")
+with c4:
+    if st.button("👥 About Us", use_container_width=True):
+        st.switch_page("pages/3_Info.py")
 st.markdown('</div>', unsafe_allow_html=True)
 
 st.markdown("""
--- pages/4_CommitAnalysis.py
+++ pages/4_CommitAnalysis.py
@@ -10,12 +10,12 @@ import sys
 
 st.set_page_config(page_title="Commit Analysis • CodeOrigin", page_icon="🔍", layout="wide")
 
-c1, c2, c3, c4, c5 = st.columns(5)
+c1, c2, c3, c4 = st.columns(4)
 with c1: st.page_link("1_Home.py", label="Home", icon="🏠")
 with c2: st.page_link("pages/2_DemoVideo.py", label="Demo", icon="🎥")
-with c3: st.page_link("pages/3_Info.py", label="About Us", icon="👥")
-with c4: st.page_link("pages/4_CommitAnalysis.py", label="CodeOrigin", icon="🔍")
-with c5: st.page_link("pages/5_RepoLink.py", label = "Repository Link", icon="⚙")
+with c3: st.page_link("pages/4_CommitAnalysis.py", label="CodeOrigin", icon="🔍")
+with c4: st.page_link("pages/3_Info.py", label="About Us", icon="👥")
+
 
 # Add lib to path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
