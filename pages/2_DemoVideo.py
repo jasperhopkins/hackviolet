bfafecf622874a60684560b23141069c1478e7a0
alexisrperry <pinkninja1109@gmail.com>
2026-02-01 00:02:43 -0500

    color, nav

--- pages/2_DemoVideo.py
+++ pages/2_DemoVideo.py
@@ -1,2 +1,13 @@
 import streamlit as st
 
+st.set_page_config(page_title="Demo • CodeOrigin", page_icon="🎥", layout="wide")
+
+c1, c2, c3, c4, c5 = st.columns(5)
+with c1: st.page_link("1_Home.py", label="Home", icon="🏠")
+with c2: st.page_link("pages/2_DemoVideo.py", label="Demo", icon="🎥")
+with c3: st.page_link("pages/3_Info.py", label="About Us", icon="👥")
+with c4: st.page_link("pages/4_CommitAnalysis.py", label="CodeOrigin", icon="🔍")
+with c5: st.page_link("pages/5_RepoLink.py", label = "Repository Link", icon="⚙")
+
+st.subheader("Demo Video")
+st.video("https://www.youtube.com/watch?v=-UGFq6jAlZg")
--- pages/5_RepoLink.py
+++ pages/5_RepoLink.py
@@ -1,1 +1,10 @@
 import streamlit as st
+
+st.set_page_config(page_title="Repo Link • CodeOrigin", page_icon="C", layout="wide")
+
+c1, c2, c3, c4, c5 = st.columns(5)
+with c1: st.page_link("1_Home.py", label="Home", icon="🏠")
+with c2: st.page_link("pages/2_DemoVideo.py", label="Demo", icon="🎥")
+with c3: st.page_link("pages/3_Info.py", label="About Us", icon="👥")
+with c4: st.page_link("pages/4_CommitAnalysis.py", label="CodeOrigin", icon="🔍")
+with c5: st.page_link("pages/5_RepoLink.py", label = "Repository Link", icon="⚙")
